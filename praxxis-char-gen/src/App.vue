81679942fc66e9d22b649043be432d4900d56453
Jason Mitchell <Jafmitch>
2022-01-15 17:22:56 -0600

    Created Vue initialization hello-world project

---
+++ praxxis-char-gen/src/App.vue
@@ -0,0 +1,20 @@
+<template>
+    <div>Hello World!</div>
+</template>
+
+<script lang="ts">
+import { Vue } from "vue-class-component";
+
+export default class App extends Vue {}
+</script>
+
+<style>
+#app {
+    font-family: Avenir, Helvetica, Arial, sans-serif;
+    -webkit-font-smoothing: antialiased;
+    -moz-osx-font-smoothing: grayscale;
+    text-align: center;
+    color: #2c3e50;
+    margin-top: 60px;
+}
+</style>
