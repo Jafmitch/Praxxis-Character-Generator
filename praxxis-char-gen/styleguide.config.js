206b1783d38a4066a07113a171d370e5c526fbab
Jason Mitchell <jafmitch@gmail.com>
2022-02-06 16:00:34 -0600

    Changed typescript configuration to javascript

--- praxxis-char-gen/styleguide.config.js
+++ praxxis-char-gen/styleguide.config.js
@@ -1,16 +1,14 @@
 module.exports = {
-  // set your styleguidist configuration here
-  title: "Default Style Guide",
-  // components: 'src/components/**/[A-Z]*.vue',
-  // defaultExample: true,
-  // sections: [
-  //   {
-  //     name: 'First Section',
-  //     components: 'src/components/**/[A-Z]*.vue'
-  //   }
-  // ],
-  // webpackConfig: {
-  //   // custom config goes here
-  // },
-  exampleMode: "expand",
+    // set your styleguidist configuration here
+    title: "Default Style Guide",
+    // components: 'src/components/**/[A-Z]*.vue',
+    // defaultExample: true,
+    // sections: [
+    //   {
+    //     name: 'First Section',
+    //     components: 'src/components/**/[A-Z]*.vue'
+    //   }
+    // ],
+    // webpackConfig: {},
+    exampleMode: "expand",
 };
